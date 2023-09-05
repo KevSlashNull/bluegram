1e00bdbaa2b1fc20adcaa51c444c5389bf9fc5f7
Kev <kevslashnull@gmail.com>
2023-09-05 15:22:59 +0200

    Fix alignment in login

--- components/login.vue
+++ components/login.vue
@@ -18,28 +18,50 @@ async function doLogin() {
 </script>
 
 <template>
-  <div
-    class="flex gap-5 flex-col items-center justify-center w-full h-full fixed"
-  >
-    {{ error }}
+  <div class="w-full h-full mx-auto fixed top-[15%]">
+    <div
+      class="flex flex-col gap-5 items-center justify-center max-w-[500px] mx-auto"
+    >
+      {{ error }}
 
-    <h1 class="text-4xl">Bluegram</h1>
+      <h1 class="text-4xl">Bluegram</h1>
 
-    <div class="form-element">
-      <label for="identifier">Identifier</label>
-      <input type="text" id="identifier" v-model="identifier" />
+      <div class="form-element">
+        <label for="identifier">Identifier</label>
+        <input type="text" id="identifier" v-model="identifier" />
+      </div>
+
+      <div class="form-element">
+        <label for="password">App password</label>
+        <input type="password" id="password" v-model="password" />
+      </div>
+
+      <button class="mb-52 bg-blue-600 py-1 px-4 rounded-lg" @click="doLogin">
+        Login
+      </button>
+
+      <div class="flex gap-2 text-muted">
+        <nuxt-link
+          class="text-blue-600"
+          target="_blank"
+          href="https://github.com/KevSlashNull/bluegram"
+        >
+          Source
+        </nuxt-link>
+        &middot;
+        <nuxt-link
+          class="text-blue-600"
+          target="_blank"
+          href="https://github.com/KevSlashNull/bluegram/blob/main/LICENSE"
+        >
+          License
+        </nuxt-link>
+      </div>
+      <span class="text-muted text-sm max-w-[80%] text-center">
+        All personal data used in Bluegram is either stored on your device or in
+        your Bluesky server.
+      </span>
     </div>
-
-    <div class="form-element">
-      <label for="password">App password</label>
-      <input type="password" id="password" v-model="password" />
-    </div>
-
-    <button class="mb-52 bg-blue-600 py-1 px-4 rounded-lg" @click="doLogin">
-      Login
-    </button>
-
-    <a href="https://github.com/kevslashnull/bluegram"></a>
   </div>
 </template>
 
