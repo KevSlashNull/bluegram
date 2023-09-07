e769304b39926d76dd88ea0d5084284e9e3b8b51
Kev <kevslashnull@gmail.com>
2023-09-07 19:31:09 +0200

    Add fallback to home

--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,14 @@ import { agent } from "~/lib/session";
 const preferences = await usePreferences().then((r) => r.value);
 const { feeds } = await agent.app.bsky.feed
   .getFeedGenerators({
-    feeds: preferences.feeds.pinned || [],
+    feeds:
+      (preferences.feeds.pinned?.length
+        ? preferences.feeds.pinned
+        : preferences.feeds.saved) || [],
   })
   .then((r) => r.data);
 
-const selectedUri = useState<string>("currentFeed", () => feeds[0].uri);
+const selectedUri = useState<string>("currentFeed", () => feeds[0]?.uri);
 </script>
 
 <template>
@@ -17,5 +20,24 @@ const selectedUri = useState<string>("currentFeed", () => feeds[0].uri);
     :selected-uri="selectedUri"
     @select="(uri) => (selectedUri = uri)"
   />
-  <feed-list :uri="selectedUri" />
+  <feed-list v-if="selectedUri" :uri="selectedUri" />
+  <div
+    v-else
+    class="py-8 flex flex-col items-center text-center max-w-[90%] mx-auto"
+  >
+    <h2 class="text-lg font-bold mb-1">No feed available?</h2>
+    <p class="mb-5">
+      In your Bluesky client, go to the <b>My Feeds</b> menu and pin one or more
+      feeds. Alternatively, try Bluesky’s Discover feed.
+    </p>
+    <button
+      class="bg-blue-700 w-max px-3 py-1.5 rounded-lg"
+      @click="
+        selectedUri =
+          'at://did:plc:z72i7hdynmk6r22z27h6tvur/app.bsky.feed.generator/whats-hot'
+      "
+    >
+      Try the Discover feed
+    </button>
+  </div>
 </template>
